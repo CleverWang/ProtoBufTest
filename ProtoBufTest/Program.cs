ab4aba239be86b4e9029a6682562395eeca6e83c
Wang Cong <1330792337@qq.com>
2022-08-18 21:31:01 +0800

    添加项目文件。

---
+++ ProtoBufTest/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ProtoBufTest
+{
+    using Google.Protobuf;
+    using ProtoBufTest.ProtoBuf;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var helloWorld = new HelloWorld()
+            {
+                Message = "Hello World!"
+            };
+            var helloProtoBuf = new HelloProtoBuf()
+            {
+                Message = "Hello ProtoBuf!"
+            };
+
+            Console.WriteLine($"helloWorld: {helloWorld}");
+            Console.WriteLine($"helloProtoBuf: {helloProtoBuf}");
+
+            using (var stream = new FileStream("HelloWorld.dat", FileMode.OpenOrCreate))
+            {
+                helloWorld.WriteTo(stream);
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var helloWorldFromStream = HelloWorld.Parser.ParseFrom(stream);
+                Console.WriteLine($"helloWorldFromStream: {helloWorldFromStream}");
+            }
+
+            using (var stream = new FileStream("HelloProtoBuf.dat", FileMode.OpenOrCreate))
+            {
+                helloProtoBuf.WriteTo(stream);
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var helloProtoBufFromStream = HelloWorld.Parser.ParseFrom(stream);
+                Console.WriteLine($"helloProtoBufFromStream: {helloProtoBufFromStream}");
+            }
+        }
+    }
+}
